e4e06917f07d07c419e721b4d1911ddebb4ea026
nqbao11 <nqbao11.01@gmail.com>
2023-03-21 12:46:52 +0000

    feat: nginx setup

---
+++ setup/nginx/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git clone https://github.com/nginxinc/kubernetes-ingress.git --branch v3.0.2
+cd kubernetes-ingress/deployments
+kubectl apply -f common/ns-and-sa.yaml
+kubectl apply -f rbac/rbac.yaml
+kubectl apply -f common/default-server-secret.yaml
+kubectl apply -f common/nginx-config.yaml
+kubectl apply -f common/ingress-class.yaml
+kubectl apply -f common/crds/k8s.nginx.org_virtualservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_virtualserverroutes.yaml
+kubectl apply -f common/crds/k8s.nginx.org_transportservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_policies.yaml
+kubectl apply -f daemon-set/nginx-ingress.yaml
+
+cd ../..
+rm -rf kubernetes-ingress
