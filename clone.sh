4179442884a248756b6bfbba1717d503c1b330ef
nqbao11 <nqbao11.01@gmail.com>
2023-03-27 04:45:23 +0000

    fix: clone.sh

--- clone.sh
+++ clone.sh
@@ -1,8 +1,8 @@
 #/bin/sh
-git clone https://github.com/FUnigrad/funiverse-authen-service 3-BE-authen
-git clone https://github.com/FUnigrad/funiverse-web-sa 6-FE-sa
-git clone https://github.com/FUnigrad/funiverse-app-service 4-BE-app
-git clone https://github.com/FUnigrad/funiverse-landing 5-FE-landing
-git clone https://github.com/FUnigrad/funiverse-web-admin 8-FE-webadmin
-git clone https://github.com/FUnigrad/funiverse-web-app 7-FE-webapp
-git clone https://github.com/FUnigrad/funiverse-workspace-service 2-BE-workspace
+git clone https://github.com/FUnigrad/funiverse-authen-service ../3-BE-authen
+git clone https://github.com/FUnigrad/funiverse-web-sa ../6-FE-sa
+git clone https://github.com/FUnigrad/funiverse-app-service ../4-BE-app
+git clone https://github.com/FUnigrad/funiverse-landing ../5-FE-landing
+git clone https://github.com/FUnigrad/funiverse-web-admin ../8-FE-webadmin
+git clone https://github.com/FUnigrad/funiverse-web-app ../7-FE-webapp
+git clone https://github.com/FUnigrad/funiverse-workspace-service ../2-BE-workspace
